063f09822595e834a5934bc43606f5bdce7ec20d
antoniodi <antonio4793@hotmail.com>
2016-04-27 12:02:27 -0500

    Consuming a RESTful Web Service with AngularJS
    Ya es posible consumir recursos en formato json desde un servicio web
    RESful

---
+++ controller/connection.js
@@ -0,0 +1,6 @@
+function Ruta($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        success(function(data) {
+            $scope.greeting = data;
+        });
+}
