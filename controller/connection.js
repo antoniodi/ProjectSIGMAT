bdafbbb0a59f3383dd6a10b329e002cbff5ec9cc
antoniodi <antonio4793@hotmail.com>
2016-05-02 13:40:40 -0500

    consume servicio Restfull y genera tabla en el layoud

--- controller/connection.js
+++ controller/connection.js
@@ -1,6 +1,43 @@
+angular.module('myApp', [])
+
+.controller('FirstCtrl', function($scope,$http) {
+$http.get('http://localhost:8001/data/coordenadas.json').
+    success(function(data) {
+        $scope.coordenadas = data;
+        console.log(data[0].Nombre);
+    });
+});
+
+
+/*
+.controller('FirstCtrl', ['$scope', function($scope) {
+   console.log("entro a mi controller");
+}]);
+*/
+/*
+function Ruta($scope, $http) {
+    $http.get('http://localhost:8001/data/coordenadas.json').
+        success(function(data) {
+            $scope.coordenadas = data;
+            console.log(data[0].Nombre);
+        });
+}
+*/
+/*
+myApp.controller('FirstCtrl', function($scope,$http) {
+$http.get('http://localhost:8001/data/coordenadas.json').
+    success(function(data) {
+        $scope.coordenadas = data;
+        console.log(data[0].Nombre);
+    });
+});
+*/
+
+/*
 function Ruta($scope, $http) {
     $http.get('http://rest-service.guides.spring.io/greeting').
         success(function(data) {
             $scope.greeting = data;
         });
 }
+*/
